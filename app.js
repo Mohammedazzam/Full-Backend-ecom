f0187beb54e7f9355a313bb6c2c68d26744999e1
Mohammedazzam <mohammed.mojahed93@gmail.com>
2025-08-31 21:25:40 +0300

    للتعامل مع البيانات + تحميل مكتبةbodyParser

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
 require("dotenv").config();
+const bodyParser = require('body-parser');
 
 
 const api = process.env.API_URL;
 
-//https://localhost:3000/api/v1
-app.get(api + "/products", (req, res) => {
 
+
+
+//https://localhost:3000/api/v1
+app.get(`${api}products`, (req, res) => {
+    const products = {
+        id: 1,
+        name: "iphone 14",
+        image: "image 1",
+    }
     res.send("Hello Products");
+});
+app.post(`${api}products`, (req, res) => {
+    const newProduct = req.body
+    console.log(products)
+
+    res.send(newProduct);
 });
 
 app.listen(3000, () => {
